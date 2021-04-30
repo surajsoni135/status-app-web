f976421ccb3535e1f0f21a21c2ae9809ad5425b7
Suraj Soni <surajsoni135@gmail.com>
2021-05-01 01:42:12 +0530

    change in user model

--- src/models/user_models.go
+++ src/models/user_models.go
@@ -2,6 +2,6 @@ package models
 
 type User struct {
 	MobileNo      string `json:"mobileNo"`
-	U_Id          string `json:"trillId"`
+	U_Id          string `json:"u_id"`
 	LastLoginTime string `json:"lastLoginTime"`
 }
